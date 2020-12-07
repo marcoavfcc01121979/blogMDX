320e0d7619668fcb654ab776fbc2a843708efa9e
marcoavfcc01121979 <marcoavfcc@gmail.com>
2020-12-07 20:58:33 -0300

    newslleter

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -6,7 +6,15 @@ const NewsLetter = () => {
       <div className="page-center">
         <h2>Get all the latest stories to your inbox</h2>
         <h4>I write to my friends every few weeks</h4>
-        <form className="contact-form">
+        <form className="contact-form"
+          name="testing-contact"
+          method="post"
+          netlify-honeypot="bot-field"
+          data-netlify="true"
+          action="/success"
+        >
+          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="testing-contact" />
           <input type="text" name="name" placeholder="Your name" className="form-control" />
           <input type="email" name="email" placeholder="Your email" className="form-control" />
           <button type="submit" className="btn form-control submit btn">
